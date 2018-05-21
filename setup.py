95b9760c101aa65f33a1d62af54282ffd4d4b3b7
Taisuke Yamada <tai@rakugaki.org>
2018-05-21 09:02:09 +0000

    Minor description update.

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup 
 setup(name='ucdev',
       version='0.0.1',
-      description='Library to access Nordic nRF24L01 wireless tranceiver over Cypress CY7C65211 USB-to-GPIO/SPI/I2C bus bridge.',
+      description='Library to access Nordic nRF24L01 wireless tranceiver over Cypress CY7C65211 USB-to-UART/I2C/SPI/GPIO bus bridge.',
       author='Taisuke Yamada',
       author_email='tai.remove-if-not-spam@rakugaki.org',
       license='Python',
