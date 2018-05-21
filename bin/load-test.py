97e490dc817e1fcd910f1301c39f093cede3d117
Taisuke Yamada <tai@rakugaki.org>
2018-05-21 09:13:46 +0000

    Added tool to test if Cypress DLL can be loaded.

---
+++ bin/load-test.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""
+To run and test this script, plug CY7C65211 in to the USB port
+
+"""
+
+from __future__ import print_function
+
+import sys, os
+import time
+from threading import *
+from IPython import embed
+
+from cy7c65211 import *
+from nrf24 import *
+
+dll = "cyusbserial"
+lib = CyUSBSerial(lib = dll)
+
+for i in lib.find(vid=0x04b4, pid=0x0004):
+  print(i)
+
+embed()
