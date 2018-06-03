b80faef3ee666bd3c13a0d387d15d68a33be33ff
Taisuke Yamada <tai@rakugaki.org>
2018-06-03 11:04:56 +0000

    Added dependencies

--- setup.py
+++ setup.py
@@ -1,8 +1,15 @@
-from setuptools import setup 
+from setuptools import setup
 setup(name='ucdev',
-      version='0.0.1',
+      version='0.0.2',
       description='Library to access Nordic nRF24L01 wireless tranceiver over Cypress CY7C65211 USB-to-UART/I2C/SPI/GPIO bus bridge.',
+      url='https://github.com/tai/python-ucdev/',
       author='Taisuke Yamada',
-      author_email='tai.remove-if-not-spam@rakugaki.org',
+      author_email='tai@remove-if-not-spam.rakugaki.org',
       license='Python',
-      packages=['cy7c65211', 'nrf24'])
+      packages=['ucdev'],
+      install_requires=[
+          'bitstring',
+          'cffi',
+          'IPython',
+      ]
+)
